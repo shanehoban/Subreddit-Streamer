88409b2b93900f0ec7c1d32e7dec8bf3fcf1fcce
Shane Hoban <shanehoban@gmail.com>
2022-10-26 14:17:53 +0100

    fixing build

--- src/router/index.js
+++ src/router/index.js
@@ -3,14 +3,27 @@ import VueRouter from 'vue-router'
 
 // App
 import App from '@/views/App.vue'
+import Subreddit from '@/views/Subreddit.vue'
+import Streams from '@/views/Streams.vue'
 import Auth from '@/views/Auth.vue'
 
 Vue.use(VueRouter)
 
 let routes = [
   // This is our non logged in 404 handler. It will match everything that is not above it
-  { path: '/', component: App, name: 'App' },
   { path: '/auth', component: Auth },
+
+  { 
+    path: '/',
+    component: App,
+    name: 'App',
+    
+    children: [
+      { path: '/', component: Subreddit, name: 'Subreddit' },
+      { path: '/stream', component: Streams, name: 'Streams' },
+    ]
+  },
+
   { path: '*', redirect: '/'}, // 4 oh 4
 ]
 
